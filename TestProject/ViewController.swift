295c57e3eaf2f2ab8c8a2b8ecc035689a103c16d
Gerdjan van Hagen <gerdjan.van.hagen@sogeti.com>
2017-03-27 10:11:00 +0200

    Initial commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Gerdjan van Hagen on 27/03/17.
+//  Copyright © 2017 Sogeti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
